8ddb10603b25374963573acef60394e86f7feb82
Sebastian <sebastian.mazur.p@gmail.com>
2023-09-29 10:50:43 +0200

    filtered by location

--- src/components/FilterSection/FilterSection.tsx
+++ src/components/FilterSection/FilterSection.tsx
@@ -5,12 +5,20 @@ import { Checkbox } from '../Checkbox/Checkbox';
 interface FilterSectionProps {
    title: string;
    options: string[];
-   jobTypes: string[];
-
+   jobTypes?: string[];
+   locations?: string[];
+   setLocations?: any;
    setJobTypes?: any;
 }
 
-export const FilterSection = ({ title, options, jobTypes, setJobTypes }: FilterSectionProps) => {
+export const FilterSection = ({
+   title,
+   options,
+   jobTypes,
+   setJobTypes,
+   locations,
+   setLocations,
+}: FilterSectionProps) => {
    return (
       <div className={styles.filter_section_wrapper}>
          <p className={styles.filter_section_title}>{title}</p>
@@ -21,6 +29,8 @@ export const FilterSection = ({ title, options, jobTypes, setJobTypes }: FilterS
                   label={option}
                   jobTypes={jobTypes}
                   setJobTypes={setJobTypes}
+                  locations={locations}
+                  setLocations={setLocations}
                />
             ))}
          </div>
